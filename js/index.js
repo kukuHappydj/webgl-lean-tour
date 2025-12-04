b36db42fee3ebe62a73761a35a9033227aae09d6
leviRa3 <levi@wizmacau.com>
2025-12-04 10:00:01 +0800

    add function createAddSetupTexture

--- js/index.js
+++ js/index.js
@@ -64,6 +64,16 @@ function activeLocation(gl, program, position) {
     gl.enableVertexAttribArray(positionAttributeLocation);
     return positionAttributeLocation;
 }
+function createAndSetupTexture(gl) {
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    // 设置材质，这样我们可以对任意大小的图像进行像素操作
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    return texture;
+}
 function main(img) {
     console.log(img);
     const canvas = document.querySelector("#c");
--- src/index.ts
+++ src/index.ts
@@ -76,6 +76,18 @@ function activeLocation(
   gl.enableVertexAttribArray(positionAttributeLocation);
   return positionAttributeLocation;
 }
+function createAndSetupTexture(gl: WebGLRenderingContext) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // 设置材质，这样我们可以对任意大小的图像进行像素操作
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+  return texture;
+}
 
 function main(img: HTMLImageElement): void {
   console.log(img);
